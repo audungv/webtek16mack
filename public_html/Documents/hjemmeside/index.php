e181cc2b952cedf46b8ca20444010fe2364b4fe3
Au-Dung <audung_91@hotmail.com>
2016-10-18 22:05:14 +0200

    gjorde koden modulær

---
+++ public_html/Documents/hjemmeside/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mal</title>
+        <meta charset="utf-8" />
+        <link href="style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Cookie|Satisfy" rel="stylesheet">
+    </head>
+    
+    
+    
+    <body>
+        
+        <!-- Navigasjonsbar -->
+        <?php
+        include "navbar.html";
+        ?>
+        
+        
+        <!-- Sidens innhold -->
+        <section id="mal">
+            <h1>Tittel</h1>
+            <p>
+                Innhold
+            </p>
+        </section>
+        
+        
+        <!-- Fotnote -->
+        <?php
+        include "footer.html";
+        ?>
+    </body>
+</html>
