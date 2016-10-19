a21c6ddcb5d135247f1b52a81563d469bc4d26f1
Minh Ha <minhhd@stud.ntnu.no>
2016-10-19 20:54:11 +0200

    oppdaterte index.php

--- public_html/Documents/hjemmeside/index.php
+++ public_html/Documents/hjemmeside/index.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Mal</title>
+        <title>Magnus & Monica</title>
         <meta charset="utf-8" />
         <link href="style.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Cookie|Satisfy" rel="stylesheet">
     </head>
-    
-    
-    
+
+
+
     <body>
-        
+
         <!-- Navigasjonsbar -->
         <?php
         include "navbar.html";
         ?>
-        
-        
+
+
         <!-- Sidens innhold -->
-        <section id="mal">
-            <h1>Tittel</h1>
-            <p>
-                Innhold
-            </p>
+        <section id="index">
+            <img id="forsideBilde" src="../img/forside_bilde.jpg" alt="Magnus & Monica">
+            <h1>Magnus & Monica</h1>
+            <p>Vielse: April 2017, Lommedalen</p>
+            <p>Vielse: Juni 2017, Trondheim</p>
         </section>
-        
-        
+
+
         <!-- Fotnote -->
         <?php
         include "footer.html";
