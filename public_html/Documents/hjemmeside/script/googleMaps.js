45b060676df83cb9010d459969c9cf9ceeb3e3e8
Minh Ha <minhhd@stud.ntnu.no>
2016-11-06 19:57:23 +0100

    La til countdown2.js

--- public_html/Documents/hjemmeside/script/googleMaps.js
+++ public_html/Documents/hjemmeside/script/googleMaps.js
@@ -1,11 +1,13 @@
+//Google Maps code from Google: https://developers.google.com/maps/documentation/javascript/adding-a-google-map#key for adding Google Maps to your site
+
 //Vielse
 function vielseMap() {
-  var lommedal = {lat: 59.950574, lng: 10.501232};
-  var map = new google.maps.Map(document.getElementById('mapVielse'), {
-    zoom: 17, //111.fikse zoom
-    center: lommedal
+  var lommedal = {lat: 59.950574, lng: 10.501232}; //Område som skal vises
+  var map = new google.maps.Map(document.getElementById('mapVielse'), { //I funksjonen google.maps.Map kan man legge til funksjoner som zoom og hvor kartet skal være sentrert.
+    zoom: 17, //111.fikse zoom, Zooming på kartet
+    center: lommedal //Hvor kartet skal sentreres, her i posisjon lommedal (satt i longitude og latitude)
   });
-  var marker = new google.maps.Marker({
+  var marker = new google.maps.Marker({ //Posisjonen til markøren
     position: lommedal,
     map: map
   });
