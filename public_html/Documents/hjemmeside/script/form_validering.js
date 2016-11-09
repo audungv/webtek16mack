b331b5fd06c9c3e3391f2338665f95be2d1832b6
Kiniroki <catriona.torklep@gmail.com>
2016-11-09 17:59:57 +0100

    oppdatering

--- public_html/Documents/hjemmeside/script/form_validering.js
+++ public_html/Documents/hjemmeside/script/form_validering.js
@@ -28,7 +28,7 @@ function validatePhone(phone){
     return false;
   }
 }
-/*
+
 function validateEmail(email){
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if(re.test(email)){
@@ -92,7 +92,7 @@ function validateForm(){
   }
 }
 
-*/
+
 
 
 /*function validateForm() {
