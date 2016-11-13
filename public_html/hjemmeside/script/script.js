63129e12056bca7c1d6ad824cf14f670a9e296a3
Minh Ha Do <mhado97@gmail.com>
2016-11-13 15:20:41 +0100

    La til kommentarer som mangla (info på toppen)

--- public_html/hjemmeside/script/script.js
+++ public_html/hjemmeside/script/script.js
@@ -1,3 +1,7 @@
+//FILE NAME: script.js
+//WRITTEN BY: Au-Dung Vuong
+//WHEN: Oktober 2016
+//PURPOSE: Kode for å gjøre navigasjonsbar og fotnoten modulær slik navigasjonsbaren og fotnoten er lik for alle sider
 $(document).ready(function(){
     $.get("navbar.html", function(data){
         $("#header").html(data);
